e1e77deaad2ede979638d0124589025707eab4b4
Felipe Sere <felipesere@gmail.com>
2018-12-30 11:44:55 +0000

    Small rocket version that exposes hard-coded values

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,3 +1,56 @@
+use serde_derive::{Serialize};
+
+use ring::signature::{Ed25519KeyPair, Signature};
+use ring::rand::SystemRandom;
+use untrusted::Input;
+use base64::encode;
+
+#[derive(Serialize)]
+#[serde(transparent)]
+pub struct VoterPublicKeys {
+    value: String
+}
+
+impl <T: AsRef<str>> From<T> for VoterPublicKeys {
+    fn from(key: T) -> VoterPublicKeys {
+        VoterPublicKeys {value: key.as_ref().to_owned()}
+    }
+}
+
+#[derive(Serialize)]
+pub struct RegisteredVoters {
+    pub signature: String,
+    pub public_keys: Vec<VoterPublicKeys>,
+}
+
+
+pub struct VoterRegistryKeys {
+    ed25519: Ed25519KeyPair
+}
+
+impl VoterRegistryKeys {
+    pub fn sign<'a>(&self, keys: &[VoterPublicKeys]) -> Signature {
+        let mut msg = String::new();
+        for key in keys {
+            msg.push_str(key.value.as_ref())
+        }
+
+        self.ed25519.sign(msg.as_ref())
+    }
+
+    pub fn new() -> Self {
+        generate_initial_keypair()
+    }
+}
+
+pub fn generate_initial_keypair() -> VoterRegistryKeys {
+    let random = SystemRandom::new();
+
+    let pkcs = Ed25519KeyPair::generate_pkcs8(&random).unwrap();
+
+    VoterRegistryKeys { ed25519: Ed25519KeyPair::from_pkcs8(Input::from(&pkcs)).unwrap()}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- voter-registry/src/main.rs
+++ voter-registry/src/main.rs
@@ -1,26 +1,11 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
-use serde_derive::{Serialize};
-
 use rocket_contrib::json::Json;
 use rocket::State;
-use ring::signature::{Ed25519KeyPair, Signature};
-use ring::rand::SystemRandom;
-use untrusted::Input;
 use base64::encode;
 
-#[derive(Serialize)]
-#[serde(transparent)]
-struct VoterPublicKeys {
-    value: String
-}
-
-#[derive(Serialize)]
-struct RegisteredVoters {
-    signature: String,
-    public_keys: Vec<VoterPublicKeys>,
-}
+use shared::{VoterRegistryKeys, VoterPublicKeys, RegisteredVoters, generate_initial_keypair};
 
 #[post("/voter_list")]
 fn register_new_voter() -> &'static str {
@@ -31,7 +16,7 @@ fn register_new_voter() -> &'static str {
 fn index(registry_keys: State<VoterRegistryKeys>) -> Json<RegisteredVoters> {
     let keys = vec!["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINALgioqnUNxPH6VsSlvfibXdXICc1C8u4tJE7aOu9Og"];
 
-    let public_keys = keys.iter().map(|k| VoterPublicKeys {value: (*k).to_owned()}).collect::<Vec<_>>();
+    let public_keys = keys.iter().map(|k| VoterPublicKeys::from(k)).collect::<Vec<_>>();
     let signed_keys = registry_keys.sign(&public_keys[..]);
 
     let signature = encode(signed_keys.as_ref());
@@ -48,44 +33,4 @@ fn main() {
         .manage(generate_initial_keypair())
         .mount("/", routes![index, register_new_voter])
         .launch();
-}
-
-struct VoterRegistryKeys {
-    ed25519: Ed25519KeyPair
-}
-
-impl VoterRegistryKeys {
-    fn sign<'a>(&self, keys: &[VoterPublicKeys]) -> Signature {
-        let mut msg = String::new();
-        for key in keys {
-            msg.push_str(key.value.as_ref())
-        }
-
-        self.ed25519.sign(msg.as_ref())
-    }
-}
-
-fn generate_initial_keypair() -> VoterRegistryKeys {
-    let random = SystemRandom::new();
-
-    let pkcs = Ed25519KeyPair::generate_pkcs8(&random).unwrap();
-
-    VoterRegistryKeys { ed25519: Ed25519KeyPair::from_pkcs8(Input::from(&pkcs)).unwrap()}
-}
-
-#[cfg(test)]
-mod tests {
-    use ring::signature::Ed25519KeyPair;
-    use ring::rand::SystemRandom;
-    use untrusted::Input;
-
-    #[test]
-    fn generates_a_keypair() {
-        let random = SystemRandom::new();
-
-        let pkcs = Ed25519KeyPair::generate_pkcs8(&random).unwrap();
-
-        let pair = Ed25519KeyPair::from_pkcs8(Input::from(&pkcs)).unwrap();
-    }
-
 }
